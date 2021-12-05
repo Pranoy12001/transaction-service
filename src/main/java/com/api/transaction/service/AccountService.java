b4ecdf4e9ce6f9f5408aa8d19e0381399bdb3dd7
pranoy12001 <pranoy.mbstu12001@gmail.com>
2021-12-05 13:35:01 +0600

    updated code

--- src/main/java/com/api/transaction/service/AccountService.java
+++ src/main/java/com/api/transaction/service/AccountService.java
@@ -1,10 +1,13 @@
 package com.api.transaction.service;
 
-import com.api.transaction.model.Transaction;
+import com.api.transaction.model.TransactionPayload;
 import org.springframework.http.ResponseEntity;
 
 import java.util.Map;
 
+/**
+ * Account Service interface
+ */
 public interface AccountService {
-    public ResponseEntity<Map> updateAccount(Transaction transaction);
+    public ResponseEntity<Map> updateAccount(TransactionPayload payload);
 }
--- src/main/java/com/api/transaction/service/impl/AccountServiceImpl.java
+++ src/main/java/com/api/transaction/service/impl/AccountServiceImpl.java
@@ -1,10 +1,13 @@
-package com.api.transaction.service;
+package com.api.transaction.service.impl;
 
 import com.api.transaction.exception.BackendException;
+import com.api.transaction.exception.ClientException;
 import com.api.transaction.model.Account;
 import com.api.transaction.model.Transaction;
+import com.api.transaction.model.TransactionPayload;
 import com.api.transaction.repository.AccountRepository;
 import com.api.transaction.repository.TransactionRepository;
+import com.api.transaction.service.AccountService;
 import com.api.transaction.util.DecoderUtil;
 import com.api.transaction.validator.AccountValidator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,25 +20,39 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.api.transaction.constant.AccountConstant.*;
+
+/**
+ * Account Service implementation class
+ */
 @Service
 public class AccountServiceImpl implements AccountService {
-    private DecoderUtil decoderUtil;
     private AccountRepository accountRepository;
     private TransactionRepository transactionRepository;
 
+    /**
+     * autowired constructor
+     *
+     * @param accountRepository
+     * @param transactionRepository
+     */
     @Autowired
-    public AccountServiceImpl(DecoderUtil decoderUtil, AccountRepository accountRepository,
-                              TransactionRepository transactionRepository) {
-        this.decoderUtil = decoderUtil;
+    public AccountServiceImpl(AccountRepository accountRepository, TransactionRepository transactionRepository) {
         this.accountRepository = accountRepository;
         this.transactionRepository = transactionRepository;
     }
 
+    /**
+     * perform the update operation
+     *
+     * @param payload of the request {@link TransactionPayload}
+     * @return response entity {@link  ResponseEntity}
+     */
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public ResponseEntity<Map> updateAccount(Transaction transaction) {
+    public ResponseEntity<Map> updateAccount(TransactionPayload payload) {
         try {
-            decoderUtil.decodeRequest(transaction);
+            Transaction transaction = DecoderUtil.decodeRequest(payload);
             Account sourceAccount =
                     accountRepository.findByAccountNumber(transaction.getSourceAccountNumber());
             Account destinationAccount =
@@ -43,24 +60,40 @@ public class AccountServiceImpl implements AccountService {
             transferAmountAndUpdateTransaction(transaction, sourceAccount, destinationAccount);
             return new ResponseEntity<>(prepareSuccessResponse(), HttpStatus.OK);
         } catch (Exception ex) {
-            BackendException backendException =
-                    BackendException.create("503", "service unavailable", ex);
-            Map<String, String> errorMap = new HashMap<>();
-            errorMap.put("code", backendException.getCode());
-            errorMap.put("message", backendException.getMessage());
-            return new ResponseEntity<>(errorMap, HttpStatus.SERVICE_UNAVAILABLE);
+            if (ex instanceof ClientException) {
+                ClientException clientException =
+                        ClientException.create(String.valueOf(HttpStatus.BAD_REQUEST.value()), INVALID_REQUEST_MESSAGE, ex);
+                Map<String, String> errorMap = new HashMap<>();
+                errorMap.put(CODE, clientException.getCode());
+                errorMap.put(MESSAGE, clientException.getMessage());
+                return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+            } else if (ex instanceof BackendException) {
+                BackendException backendException =
+                        BackendException.create(String.valueOf(HttpStatus.SERVICE_UNAVAILABLE.value()), ex.getMessage(), ex);
+                Map<String, String> errorMap = new HashMap<>();
+                errorMap.put(CODE, backendException.getCode());
+                errorMap.put(MESSAGE, backendException.getMessage());
+                return new ResponseEntity<>(errorMap, HttpStatus.SERVICE_UNAVAILABLE);
+            } else {
+                BackendException backendException =
+                        BackendException.create(String.valueOf(HttpStatus.SERVICE_UNAVAILABLE.value()), SERVICE_UNAVAILABLE_MESSAGE, ex);
+                Map<String, String> errorMap = new HashMap<>();
+                errorMap.put(CODE, backendException.getCode());
+                errorMap.put(MESSAGE, backendException.getMessage());
+                return new ResponseEntity<>(errorMap, HttpStatus.SERVICE_UNAVAILABLE);
+            }
         }
     }
 
     private Map<String, String> prepareSuccessResponse() {
         Map<String, String> responseMap = new HashMap<>();
-        responseMap.put("code", "200");
-        responseMap.put("message", "transaction happened successfully");
+        responseMap.put(CODE, String.valueOf(HttpStatus.OK.value()));
+        responseMap.put(MESSAGE, SUCCESSFUL_TRANSACTION_MESSAGE);
         return responseMap;
     }
 
     private void transferAmountAndUpdateTransaction(Transaction transaction, Account sourceAccount, Account destinationAccount) {
-        if (transaction.getTransactionType().equals("TRANSFER")) {
+        if (transaction.getTransactionType().equals(TRANSFER)) {
             AccountValidator.validate(sourceAccount, destinationAccount, transaction);
             sourceAccount.setBalance(sourceAccount.getBalance() - transaction.getAmount());
             destinationAccount.setBalance(destinationAccount.getBalance() + transaction.getAmount());
@@ -68,7 +101,7 @@ public class AccountServiceImpl implements AccountService {
             accountRepository.save(destinationAccount);
             transaction.setTransactionTime(new Date());
             transactionRepository.save(transaction);
-        } else if (transaction.getTransactionType().equals("REVERSE")) {
+        } else if (transaction.getTransactionType().equals(REVERSE)) {
             AccountValidator.validate(sourceAccount, destinationAccount, transaction);
             sourceAccount.setBalance(sourceAccount.getBalance() + transaction.getAmount());
             destinationAccount.setBalance(destinationAccount.getBalance() - transaction.getAmount());
