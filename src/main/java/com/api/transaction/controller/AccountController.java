97dd7fb4d357339bb2d6a1181194c1ae2b0a7dd1
pranoy12001 <pranoy.mbstu12001@gmail.com>
2021-12-05 13:34:23 +0600

    refactor code

--- src/main/java/com/api/transaction/controller/AccountController.java
+++ src/main/java/com/api/transaction/controller/AccountController.java
@@ -1,11 +1,10 @@
 package com.api.transaction.controller;
 
 import com.api.transaction.exception.ClientException;
-import com.api.transaction.model.Transaction;
+import com.api.transaction.model.TransactionPayload;
 import com.api.transaction.service.AccountService;
 import com.api.transaction.validator.RequestValidator;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,43 +15,57 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.api.transaction.constant.AccountConstant.*;
+
+/**
+ * Account Controller
+ */
 @Controller
 public class AccountController {
     private AccountService accountService;
-    ObjectMapper mapper;
 
-    public AccountController(AccountService accountService, ObjectMapper mapper) {
+    /**
+     * Autowired constructor
+     *
+     * @param accountService
+     */
+    @Autowired
+    public AccountController(AccountService accountService) {
         this.accountService = accountService;
-        this.mapper = mapper;
     }
 
+    /**
+     * this controller is used for to test the application is running or not
+     *
+     * @return response {@link ResponseEntity}
+     */
     @GetMapping("/test-application")
     public ResponseEntity<String> testApplication() {
         return new ResponseEntity<>("Application is running...", HttpStatus.OK);
     }
 
+    /**
+     * this controller is used to trigger transaction
+     *
+     * @param payload payload of the request{@link TransactionPayload}
+     * @return code and message as a Map {@link ResponseEntity}
+     */
     @PostMapping("/transfer-money")
-    public ResponseEntity<Map> triggerTransaction(@RequestBody String body) {
-        Transaction transaction;
+    public ResponseEntity<Map> triggerTransaction(@RequestBody TransactionPayload payload) {
         try {
-            transaction = mapper.readValue(body, Transaction.class);
-        } catch (JsonProcessingException ex) {
-            return getErrorResponseEntity(ex);
-        }
-
-        try {
-            RequestValidator.validate(transaction);
+            RequestValidator.validate(payload);
         } catch (ClientException ex) {
             return getErrorResponseEntity(ex);
         }
-        return accountService.updateAccount(transaction);
+        return accountService.updateAccount(payload);
     }
 
     private ResponseEntity<Map> getErrorResponseEntity(Exception ex) {
-        ClientException clientException = ClientException.create("400", "invalid request", ex);
+        ClientException clientException =
+                ClientException.create(String.valueOf(HttpStatus.BAD_REQUEST.value()), INVALID_REQUEST_MESSAGE, ex);
         Map<String, String> errorMap = new HashMap<>();
-        errorMap.put("code", clientException.getCode());
-        errorMap.put("message", clientException.getMessage());
+        errorMap.put(CODE, clientException.getCode());
+        errorMap.put(MESSAGE, clientException.getMessage());
         return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
     }
 }
