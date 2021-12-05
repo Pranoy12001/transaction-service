837c90dcf2f961e09385975b27bae5c6ed3e3eef
pranoy12001 <pranoy.mbstu12001@gmail.com>
2021-12-05 13:35:24 +0600

    refactor code

--- src/main/java/com/api/transaction/validator/AccountValidator.java
+++ src/main/java/com/api/transaction/validator/AccountValidator.java
@@ -3,23 +3,30 @@ package com.api.transaction.validator;
 import com.api.transaction.exception.BackendException;
 import com.api.transaction.model.Account;
 import com.api.transaction.model.Transaction;
+import org.springframework.http.HttpStatus;
 
 import java.util.Objects;
+
+import static com.api.transaction.constant.AccountConstant.*;
 
 public class AccountValidator {
     public static void validate(Account sourceAccount, Account destinationAccount,
                                 Transaction transaction) {
         if (Objects.isNull(sourceAccount) || Objects.isNull(destinationAccount)) {
-            throw BackendException.create("503", "source/destination account not available", null);
+            throw BackendException
+                    .create(String.valueOf(HttpStatus.SERVICE_UNAVAILABLE.value()), ACCOUNT_NOT_AVAILABLE_MESSAGE, null);
         } else if (sourceAccount.getStatus() != 1 || destinationAccount.getStatus() != 1){
-            throw BackendException.create("503", "source/destination account not active", null);
+            throw BackendException
+                    .create(String.valueOf(HttpStatus.SERVICE_UNAVAILABLE.value()), ACCOUNT_NOT_ACTIVE_MESSAGE, null);
         } else {
-            if (transaction.getTransactionType().equals("TRANSFER")
+            if (transaction.getTransactionType().equals(TRANSFER)
                     && sourceAccount.getBalance() < transaction.getAmount()) {
-                throw BackendException.create("503", "insufficient balance", null);
-            } else if (transaction.getTransactionType().equals("REVERSE")
+                throw BackendException
+                        .create(String.valueOf(HttpStatus.SERVICE_UNAVAILABLE.value()), INSUFFICIENT_MESSAGE, null);
+            } else if (transaction.getTransactionType().equals(REVERSE)
                     && destinationAccount.getBalance() < transaction.getAmount()) {
-                throw BackendException.create("503", "insufficient balance", null);
+                throw BackendException
+                        .create(String.valueOf(HttpStatus.SERVICE_UNAVAILABLE.value()), INSUFFICIENT_MESSAGE, null);
             }
         }
     }
--- src/main/java/com/api/transaction/validator/RequestValidator.java
+++ src/main/java/com/api/transaction/validator/RequestValidator.java
@@ -1,22 +1,36 @@
 package com.api.transaction.validator;
 
 import com.api.transaction.exception.ClientException;
-import com.api.transaction.model.Transaction;
+import com.api.transaction.model.TransactionPayload;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
 
 import java.util.Objects;
 
+import static com.api.transaction.constant.AccountConstant.INVALID_REQUEST_MESSAGE;
+
+/**
+ * Request Validator
+ */
 public class RequestValidator {
-    public static void validate(Transaction transaction) {
-        if (Objects.isNull(transaction)) {
-            throw ClientException.create("400", "invalid request", null);
-        } else if (Objects.isNull(transaction.getRequestId())
-                || Objects.isNull(transaction.getRequester())
-                || Objects.isNull(transaction.getEncodedTransactionType())
-                || Objects.isNull(transaction.getEncodedTransactionType())
-                || Objects.isNull(transaction.getEncodedAmount())
-                || Objects.isNull(transaction.getEncodedDestinationAccountNumber())
-                || Objects.isNull(transaction.getNote())) {
-            throw ClientException.create("400", "invalid request", null);
+    /**
+     * validate the incoming payload
+     *
+     * @param payload
+     */
+    public static void validate(TransactionPayload payload) {
+        if (Objects.isNull(payload)) {
+            throw ClientException
+                    .create(String.valueOf(HttpStatus.BAD_REQUEST.value()), INVALID_REQUEST_MESSAGE, null);
+        } else if (StringUtils.isBlank(payload.getRequestId())
+                || StringUtils.isBlank(payload.getRequester())
+                || StringUtils.isBlank(payload.getTransactionType())
+                || StringUtils.isBlank(payload.getSourceAccountNumber())
+                || StringUtils.isBlank(payload.getAmount())
+                || StringUtils.isBlank(payload.getDestinationAccountNumber())
+                || StringUtils.isBlank(payload.getNote())) {
+            throw ClientException
+                    .create(String.valueOf(HttpStatus.BAD_REQUEST.value()), INVALID_REQUEST_MESSAGE, null);
         }
     }
 }
