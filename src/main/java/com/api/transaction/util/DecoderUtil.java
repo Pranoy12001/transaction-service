73f9dd9edbab0c89e3729392d06e626cd704a532
pranoy12001 <pranoy.mbstu12001@gmail.com>
2021-12-05 13:35:12 +0600

    updated logic

--- src/main/java/com/api/transaction/util/DecoderUtil.java
+++ src/main/java/com/api/transaction/util/DecoderUtil.java
@@ -1,27 +1,69 @@
 package com.api.transaction.util;
 
+import com.api.transaction.exception.ClientException;
 import com.api.transaction.model.Transaction;
+import com.api.transaction.model.TransactionPayload;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.util.Base64;
 
+import static com.api.transaction.constant.AccountConstant.*;
+
+/**
+ * Decoder Util
+ */
 @Component
 public class DecoderUtil {
-    public void decodeRequest(Transaction transaction) {
+
+    /**
+     * this method decode the payload and prepare transaction entity
+     *
+     * @param payload
+     * @return
+     */
+    public static Transaction decodeRequest(TransactionPayload payload) {
+        Transaction transaction = new Transaction();
+        transaction.setRequestId(payload.getRequestId());
+        transaction.setRequester(payload.getRequester());
         byte[] transactionType =
-                Base64.getDecoder().decode(transaction.getEncodedTransactionType());
+                Base64.getDecoder().decode(payload.getTransactionType());
         transaction.setTransactionType(new String(transactionType));
+        if (!isValidTransactionType(transaction)) {
+            throw ClientException
+                    .create(String.valueOf(HttpStatus.BAD_REQUEST), INVALID_REQUEST_MESSAGE, null);
+        }
 
         byte[] sourceAccountNumber =
-                Base64.getDecoder().decode(transaction.getEncodedSourceAccountNumber());
+                Base64.getDecoder().decode(payload.getSourceAccountNumber());
         transaction.setSourceAccountNumber(new String(sourceAccountNumber));
 
         byte[] amount =
-                Base64.getDecoder().decode(transaction.getEncodedAmount());
-        transaction.setAmount(Double.parseDouble(new String(amount)));
+                Base64.getDecoder().decode(payload.getAmount());
+        transaction.setAmount(parseDouble(new String(amount)));
 
         byte[] destinationAccountNumber =
-                Base64.getDecoder().decode(transaction.getEncodedDestinationAccountNumber());
+                Base64.getDecoder().decode(payload.getDestinationAccountNumber());
         transaction.setDestinationAccountNumber(new String(destinationAccountNumber));
+        transaction.setNote(payload.getNote());
+        return transaction;
+    }
+
+    private static boolean isValidTransactionType(Transaction transaction) {
+        if (transaction.getTransactionType().equals(TRANSFER)) {
+            return true;
+        } else if (transaction.getTransactionType().equals(REVERSE)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static double parseDouble(String amount) {
+        try {
+            return Double.parseDouble(amount);
+        } catch (NumberFormatException ex) {
+            throw ClientException.create(String.valueOf(HttpStatus.BAD_REQUEST.value()), INVALID_REQUEST_MESSAGE, ex);
+        }
     }
 }
