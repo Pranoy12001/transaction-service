e5c47dbe6d7207e64a8f77242fda4b9a6b7ed905
pranoy12001 <pranoy.mbstu12001@gmail.com>
2021-12-05 13:34:09 +0600

    added constant file

---
+++ src/main/java/com/api/transaction/constant/AccountConstant.java
@@ -0,0 +1,17 @@
+package com.api.transaction.constant;
+
+/**
+ * Account Constant
+ */
+public class AccountConstant {
+    public static final String INVALID_REQUEST_MESSAGE = "invalid request";
+    public static final String SERVICE_UNAVAILABLE_MESSAGE = "service unavailable";
+    public static final String SUCCESSFUL_TRANSACTION_MESSAGE = "transaction happened successfully";
+    public static final String CODE = "code";
+    public static final String MESSAGE = "message";
+    public static final String TRANSFER = "TRANSFER";
+    public static final String REVERSE = "REVERSE";
+    public static final String ACCOUNT_NOT_AVAILABLE_MESSAGE = "source/destination account not available";
+    public static final String ACCOUNT_NOT_ACTIVE_MESSAGE = "source/destination account not active";
+    public static final String INSUFFICIENT_MESSAGE = "insufficient balance";
+}
